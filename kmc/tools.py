1f9f58d204b64c687bf0382facfa6f2c930fad69
LeonardoESousa <leonardo.sousa137@gmail.com>
2022-09-28 15:29:15 +0200

    improved dashboard

--- kmc/tools.py
+++ kmc/tools.py
@@ -1,14 +1,7 @@
 # Aux. code for analysis 
 import numpy as np
-import random
-from matplotlib import style
-import os
 import ipywidgets as widgets
-from IPython.display import display
-import pandas as pd
-from ipywidgets import fixed, Layout, Button, Box
-import scipy.optimize as spopt 
-import io
+
 ##############################
 ### FUNCTIONS FOR DASHBOARD ##
 ##############################
@@ -25,30 +18,24 @@ def get_bound(max_val,min_val):
     return min_val*(1+boundary_min),max_val*(1+boundary_max)
 
 ##################################################################
-#### Filtering funcs
-def get_energy(data):
-    return data.Energy
-def get_dx(data):
-    return data.DeltaX
-def get_dy(data):
-    return data.DeltaY
-def get_dz(data):
-    return data.DeltaZ
-def get_location(data):
-    return data.Location
+
 #filter some variable inside some list
 def filter_data_list(data,keyword,list_choosen):
     return data[(data[keyword].isin(list_choosen))]
+
 #filter some variable inside some numeric range
 def filter_data_range(data,keyword,values):
-    dic = {'Energy': get_energy, 'DeltaX': get_dx, 'DeltaY':get_dy, 'DeltaZ': get_dz,'Location':get_location}
-    return data[((dic[keyword](data) <= values[1]) & (dic[keyword](data) >= values[0]))] 
+    if keyword != 'Time':
+        return data[((data[keyword].to_numpy(float) <= values[1]) & (data[keyword].to_numpy(float) >= values[0]))] 
+    else:
+        return data[((data[keyword].to_numpy(float) <= 10**(12+values[1])) & (data[keyword].to_numpy(float) >= 10**(12+values[0])))]
 
 #Returns a filtered dataframe for a given particle
 #death processes, material location, energy...
-def filter_per_particle(dataframe,particle,deaths,mats,energy):
+def filter_per_particle(dataframe,particle,deaths,mats,energy,times):
     data = filter_data_list(dataframe,'Type',[particle])
     data_filtred = data
+    data_filtred = filter_data_range(data_filtred,'Time',times)
     data_filtred = filter_data_list(data_filtred,'CausaMortis',deaths)
     data_filtred = filter_data_range(data_filtred,'Location',mats)
     data_filtred = filter_data_range(data_filtred,'Energy',energy)
@@ -59,14 +46,14 @@ def filter_per_particle(dataframe,particle,deaths,mats,energy):
 
 #returns a set of widgets for some particle to compose
 #the first panel
-def get_widgets_per_particle(min_energy,max_energy,unique_mats,unique_death):
+def get_widgets_per_particle(min_energy,max_energy,unique_mats,unique_death,min_time,max_time):
     min_bound,max_bound = get_bound(max_energy,min_energy)
     energy = widgets.FloatRangeSlider(
         value=[min_energy, max_energy],
             min=min_bound,
             max=max_bound,
             step=(max_energy-min_energy)/4,
-            description='Energy:',
+            description='Energy (eV):',
             disabled=False,
             continuous_update=False,
             orientation='horizontal',
@@ -99,7 +86,19 @@ def get_widgets_per_particle(min_energy,max_energy,unique_mats,unique_death):
     #        description='Status',
     #        disabled=False,
     #    )
-    return [mats,energy,deaths]#,status]
+    times = widgets.FloatRangeSlider(
+            value=[np.log10(min_time)-12, np.log10(max_time)-12],
+            min=np.log10(min_time)-12,
+            max=np.log10(max_time)-12,
+            step=0.1,
+            description='Time: $10^x$(s)',
+            disabled=False,
+            continuous_update=False,
+            orientation='horizontal',
+            readout=True,
+            readout_format='.1f',
+        )
+    return [mats,energy,deaths,times]#,status]
 
 
 #gathers the personalized info to construct the widgets
@@ -119,17 +118,18 @@ def join_widgets(dataframe,particle):
     
 #wraps death,status,mats and enegy widgets in a single big box  
 def make_boxes(wids):
-    deaths,mats,energy = wids[2],wids[0],wids[1] #wids[3],
+    deaths,mats,energy,times = wids[2],wids[0],wids[1], wids[3],
     left_box  = widgets.VBox([deaths])#,status])
-    right_box = widgets.VBox([mats,energy])
+    right_box = widgets.VBox([mats,energy,times])
     ui = widgets.HBox([left_box,right_box])  
     return ui    
     
-def trpl(times,bin_num=100):
-    hist, bins = np.histogram(times, bins=np.logspace(-1,np.log10(max(times)), bin_num),density=True)    
+def trpl(times,bin_num=10):
+    num = int((max(times)-min(times))/bin_num)
+    hist, bins = np.histogram(times, bins=np.linspace(min(times),max(times),num),density=True)    
     bins = bins[:-1] +(bins[1:] - bins[:-1])/2
     return  hist, bins
-    
+
     
 def spectrum(dx,gran):
     num = int((max(dx)-min(dx))/gran)
